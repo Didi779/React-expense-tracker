faa17a8856126bf2b5cf938fe3142c4448fee31b
Didimalang Phele <71471501+Didi779@users.noreply.github.com>
2024-09-02 21:26:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import Header from './Header';
 import ExpenseList from './ExpenseList';
+import ExpenseForm from './ExpenseForm';
 
 function App() {
   return (
     <div>
       <Header />
+      <ExpenseForm />
       <ExpenseList />
     </div>
   );
