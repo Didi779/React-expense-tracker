ea6b096db255d0c636d1e834563e0910e4f9f41b
Didimalang Phele <71471501+Didi779@users.noreply.github.com>
2024-09-02 21:20:52 +0200

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,17 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Expense Tracker</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Add Expense</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
