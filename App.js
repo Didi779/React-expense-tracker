4265d2dc8e7946d1371203e7ae5ba663d909ebcd
Didimalang Phele <71471501+Didi779@users.noreply.github.com>
2024-09-02 21:20:05 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import Header from './Header';
+import ExpenseList from './ExpenseList';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ExpenseList />
+    </div>
+  );
+}
+
+export default App;
